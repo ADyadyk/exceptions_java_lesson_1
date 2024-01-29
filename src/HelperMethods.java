e9de485745a7da019e1bb52e57a8b36a74d20727
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-29 12:26:51 +0300

    Initial commit

---
+++ src/HelperMethods.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class HelperMethods implements HelperMethodable {
+    @Override
+    public String promptString(String message){
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+
+    @Override
+    public boolean checkLogin(String login){
+        List<String> checkSymbols = new ArrayList<>();
+        checkSymbols.add("a");
+        checkSymbols.add("b");
+        checkSymbols.add("c");
+        checkSymbols.add("d");
+        checkSymbols.add("e");
+        checkSymbols.add("0");
+        checkSymbols.add("1");
+        checkSymbols.add("_");
+
+        List<String> inputting = new ArrayList<>(Arrays.asList(login.split("")));
+        inputting.removeIf(checkSymbols::contains);
+
+        return inputting.isEmpty();
+    }
+}
